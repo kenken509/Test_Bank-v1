0bf09d269506057783291a5060f96261287b52ba
kenken509 <ariesllesis050382@gmail.com>
2024-05-15 23:29:50 +0800

    depsearch

--- resources/js/Pages/Dashboard/Department/DepartmentAll.vue
+++ resources/js/Pages/Dashboard/Department/DepartmentAll.vue
@@ -1,7 +1,14 @@
 <template>
     <DashboardLayout>
-        <div class="flex items-center sm:flex-col md:flex-row  lg:justify-between border-bot-only py-2 border border-gray-600   mb-4">
-            <span class="text-[20px] font-bold text-gray-500">Departmens Page</span>  
+        <div class="flex items-center sm:flex-col md:flex-row  lg:justify-between border-bot-only py-2 border-bot-only   mb-4">
+            <span class="text-[20px] font-bold text-gray-500">Departments Page</span> 
+            <div class="relative">
+                <input v-model="searchField"  type="text" placeholder="seach" class="rounded-md ">
+                <svg class="absolute top-3 right-2 w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div> 
+            
         </div>
         <!--TABLE-->
         <div>
@@ -27,46 +34,70 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="dep in data.departments" :key="dep.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <tr v-for="dep in filteredData" :key="dep.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ dep.name }}
                             </th>
-
-                            <td v-if="dep.divisions.length"  v-for="division in dep.divisions" :key="division.id" class="flex px-6 py-2">
-                                <span > {{ division.name }}</span>
-                                
+                            
+                            <td v-if="dep.divisions.length"   class="flex flex-col px-6 ">
+                                <span v-for="division in dep.divisions" :key="division.id" class="py-2"> {{ division.name }}</span>
                             </td>
-                            <td v-else class="flex px-6 py-4">--</td>
+                            <td v-if="!dep.divisions.length" class="flex px-6 py-4">--</td>
 
 
-                            <td class="px-6 py-4">
-                                {{ dep.department_heads }}
+                            <td  v-if="dep.department_heads.length"  class="flex flex-col px-6 ">
+                                <span v-for="depHead in dep.department_heads" :key="depHead.id" class="py-2"> {{ depHead.name }}  </span>
                             </td>
+                            <td  v-if="!dep.department_heads.length"  class="px-6 py-4">
+                                <span> -- </span>
+                            </td>
+                            
 
 
-                            <td class="px-6 py-4">
-                                {{ dep.faculty }}
+                            <td  v-if="dep.faculty.length" class="px-6 py-4">
+                                <span v-for="faculty in dep.faculty" :key="faculty.id" class="py-2"> {{ faculty.name }}  </span>
+                            </td>
+                            <td  v-if="!dep.faculty.length"  class="px-6 py-4">
+                                <span> -- </span>
                             </td>
                            
                             <td class="px-6 py-4">
                                 <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                             </td>
                         </tr>
-                        
-                        
                     </tbody>
                 </table>
             </div>
         </div>
         <!--TABLE-->
+
+        <!--PAGINATION CONTROLS-->
+        <div class="flex justify-center items-center mt-4">
+            <button @click="prevPage" :disabled="currentPage === 1" class="px-4 py-2 mx-1 bg-blue-600 text-white rounded-md disabled:opacity-50">Previous</button>
+            <span class="px-4 py-2">{{ currentPage }} / {{ totalPages }}</span>
+            <button @click="nextPage" :disabled="currentPage === totalPages" class="px-4 py-2 mx-1 bg-blue-600 text-white rounded-md disabled:opacity-50">Next</button>
+        </div>
+        <!--PAGINATION CONTROLS-->
     </DashboardLayout>
 
 </template>
 
 <script setup>
 import DashboardLayout from '../DashboardLayout.vue'
+import {computed, ref} from 'vue'
 
 const data = defineProps({
     departments:Array,
 })
+
+const searchField = ref('')
+const filteredData = computed(() => {
+  if (!searchField.value) {
+    return data.departments
+  }
+  const searchTerm = searchField.value.toLowerCase()
+  return data.departments.filter(dep => dep.name.toLowerCase().includes(searchTerm))
+})
+
+
 </script>
